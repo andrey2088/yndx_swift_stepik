0aff054a0d94bda73b71a559f5ea76b13de37acf
Andrey <andrewenka@gmail.com>
2019-07-03 00:32:59 +0300

    Project Notes created.

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by andrey on 2019-07-03.
+//  Copyright © 2019 andrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
