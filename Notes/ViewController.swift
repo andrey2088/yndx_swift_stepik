34b2dc24200610ab9c6b7df31eae16dbb60d8627
Andrey <andrewenka@gmail.com>
2019-07-06 21:27:50 +0300

    Scheme and configuration for dark mode added.

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -13,8 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        #if DARKMODE
+            self.view.backgroundColor = UIColor(red: 28.0/255.0, green: 28.0/255.0, blue: 28.0/255.0, alpha: 1)
+        #else
+            self.view.backgroundColor = UIColor(red: 242.0/255.0, green: 242.0/255.0, blue: 247.0/255.0, alpha: 1)
+        #endif
     }
-
-
 }
 
